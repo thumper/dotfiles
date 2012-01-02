47b17eed033f1e89858b6f879155bd079ca52565
Bo Adler <thumper@alumni.caltech.edu>
2012-01-01 22:42:17 -0800

    xmonad: include xmonad config

---
+++ xmonad/xmonad.hs
@@ -0,0 +1,18 @@
+import XMonad
+import XMonad.Config.Gnome
+ 
+-- Declare config preferences
+-- config_terminal = "terminator"      -- Default terminal to run
+config_focusFollowsMouse :: Bool    -- Have focus follow mouse
+config_focusFollowsMouse = True
+ 
+-- Run xmonad with the specified conifguration
+main = xmonad myConfig
+ 
+-- Use the gnomeConfig, but change a couple things
+myConfig = gnomeConfig {
+--     terminal            = config_terminal,
+    focusFollowsMouse   = config_focusFollowsMouse
+ 
+}
+
