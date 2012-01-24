0412a32ade924ae96a3f6010aa22a78d00b62ae8
Bo Adler <thumper@alumni.caltech.edu>
2012-01-23 19:29:20 -0800

    xmonad: use the Command/Windows key, instead of Option/Alt.

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,18 +1,20 @@
 import XMonad
 import XMonad.Config.Gnome
- 
+
 -- Declare config preferences
 -- config_terminal = "terminator"      -- Default terminal to run
 config_focusFollowsMouse :: Bool    -- Have focus follow mouse
 config_focusFollowsMouse = True
- 
+
 -- Run xmonad with the specified conifguration
 main = xmonad myConfig
- 
+
 -- Use the gnomeConfig, but change a couple things
 myConfig = gnomeConfig {
 --     terminal            = config_terminal,
-    focusFollowsMouse   = config_focusFollowsMouse
- 
+    focusFollowsMouse   = config_focusFollowsMouse,
+    -- use the Command key in OSX, instead of Alt/Option.
+    modMask = mod4Mask
+
 }
 
